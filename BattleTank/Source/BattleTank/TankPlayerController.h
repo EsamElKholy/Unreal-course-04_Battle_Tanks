bc5daf9b064b2499f6fb142dbd6dcdf0f37f4b49
kai <0ultrakai0@gmail.com>
2017-03-28 00:45:41 +0200

    Added TankPlayerController to use instead of the normal PlayerController Added Tank pawn and changed Tank_BP to be of that type

---
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Tank.h"
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h" // It has to be the last thing to include
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	ATank* GetControlledTank() const;	
+	
+};
