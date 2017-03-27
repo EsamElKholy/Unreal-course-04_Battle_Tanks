a937679994bcaa6dfc4aacef7f7d02ad47290f67
kai <0ultrakai0@gmail.com>
2017-03-28 01:04:11 +0200

    Added overriden BeginPlay method to TankPlayerController

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -3,6 +3,11 @@
 #include "BattleTank.h"
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay() 
+{
+	Super::BeginPlay();
+}
+
 ATank* ATankPlayerController::GetControlledTank() const 
 {
 	return Cast<ATank>(GetPawn());
--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -13,6 +13,9 @@ UCLASS()
 class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+public:
+	virtual void BeginPlay() override;
 	
 public:
 	ATank* GetControlledTank() const;	
